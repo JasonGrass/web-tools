752ec4a635f2f0cad52311c4798f6c7d336d9ac4
JasonGrass <junjiegrass@qq.com>
2023-06-12 12:20:14 +0800

    补充关于页面

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,25 @@
-import React, { memo } from 'react'
+import styled from "styled-components"
+
+import React, { memo } from "react"
 
 const About = memo(() => {
+  const url = "https://github.com/JasonGrass/web-tools"
+
   return (
-    <div>About</div>
+    <Style>
+      <a href={url} target="_blank" rel="noopener noreferrer">
+        {url}
+      </a>
+    </Style>
   )
 })
 
 export default About
+
+const Style = styled.div`
+  margin-top: 100px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
